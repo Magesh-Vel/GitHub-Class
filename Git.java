4a279a1baa2099a2e0bf6f41b103486eb6f289b3
Magesh-vel <mageshvel1745@gmail.in>
2025-08-01 12:43:59 +0530

    create new java

---
+++ Git.java
@@ -0,0 +1,9 @@
+public class Git{
+    public static void main(String[]args){
+        int a = 10;
+        int b = 30;
+        System.out.println(a+b);
+        git config --global user.name 'Magesh-Vel'
+        git config --global user.email "mageshvel1745@gmail"
+    }
+}

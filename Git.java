670254068d803c234a3b56dc1444ccafaddc0acd
Magesh-Vel <mageshvel1745@gmail.com>
2025-08-05 13:47:46 +0530

    Update Git.java
    add over project

--- Git.java
+++ Git.java
@@ -5,12 +5,14 @@ public class Git{
         System.out.println(a+b);
         git config --global user.name 'Magesh-Vel'
         git config --global user.email "mageshvel1745@gmail"
+            
 
        /* u --> add --> commit -->push
         local                                   github
 
         git add git.java
         git commit -m "message"
-        git status*/
+        git status
+        project over*/
     }
 }
